94a6f3c551d2f7792dbefb21d62f69c6c2b38bf0
Matthew Kataryniak <Matthew Kataryniak>
2016-12-13 14:49:16 -0500

    pdf test

---
+++ Documents/NJIT/2016 Fall/CS 280-005/workspace/SecondProject/Wazzup.cpp
@@ -0,0 +1,20 @@
+/*
+ * Wazzup.cpp
+ *
+ *  Created on: Sep 6, 2016
+ *      Author: mattk_000
+ */
+
+#include <iostream>
+using namespace std;
+
+int add(int x, int y){
+	int r;
+	r = x + y;
+	return r;
+}
+
+int main(){
+	string test = "Screen door slams. Mary’s dress  waves.  Like a  vision  she";
+	cout << test.size();
+}
